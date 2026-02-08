d697209bca3ea3b95e3ef9d8a59d69bd1a2deb3d
Yubin Lee <yubin8938@gmail.com>
2026-02-08 18:30:47 +0900

    Init page structures (#2)

---
+++ src/app/(afterLogin)/messages/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>메세지 페이지</main>;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+  return (
+    <div>
+      <div>이 페이지는 존재 안합니다.</div>
+      <Link href="/search">검색</Link>
+    </div>
+  );
+};
+export default NotFound;
