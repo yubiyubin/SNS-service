6d381aed0f69182d298c43134109d358f5cba346
Yubin Lee <yubin8938@gmail.com>
2026-02-12 22:14:06 +0900

    feat: Add post and follow recommendation components with styling (#4)

---
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -0,0 +1,24 @@
+import style from "./trendSection.module.css";
+import Trend from "./Trend";
+
+const TrendSection = () => {
+  return (
+    <div className={style.trendBg}>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </div>
+  );
+};
+
+export default TrendSection;
