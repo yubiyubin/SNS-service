bef0ade5e707a0a6cefb92ea22b8d693d6f89152
Yubin Lee <yubin8938@gmail.com>
2026-02-16 19:03:35 +0900

    feat: Implement explore and tweet modal components with styling and functionality (#6)

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,12 @@
+"use client";
 import style from "./trendSection.module.css";
 import Trend from "./Trend";
+import { usePathname } from "next/navigation";
 
 const TrendSection = () => {
+  const pathname = usePathname();
+
+  if (pathname === "/explore") return null;
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
