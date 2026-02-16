378c1c0129b5fe788d5698b40c5faaceff22a728
YUBIN <yubin8938@gmail.com>
2026-02-16 19:01:14 +0900

    feat: Implement explore and tweet modal components with styling and functionality

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,25 @@
+import SearchForm from "../_component/SearchForm";
+import style from "./explore.module.css";
+import Trend from "../_component/Trend";
+
 export default function Page() {
-  return <main>탐색</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
